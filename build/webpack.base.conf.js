0e3b2b077cf7d0ed4228b4d20bb5226b8366d927
xiangsongtao <280304286@163.com>
2017-11-09 12:36:37 +0800

    feat: 更新vimo版本到0.8.1

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -2,6 +2,7 @@ var path = require('path')
 var utils = require('./utils')
 var config = require('../config')
 var vueLoaderConfig = require('./vue-loader.conf')
+const EncodingPlugin = require('webpack-encoding-plugin')
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
@@ -22,7 +23,7 @@ module.exports = {
     extensions: ['.js', '.vue', '.json'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src'),
+      '@': resolve('src')
     }
   },
   module: {
@@ -63,5 +64,10 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new EncodingPlugin({
+      encoding: 'utf-8'
+    })
+  ]
 }
--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -8,7 +8,6 @@ var CopyWebpackPlugin = require('copy-webpack-plugin')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
-
 var env = config.build.env
 
 var webpackConfig = merge(baseWebpackConfig, {
